ddfca39a1686546c89a4a3b05b5a16217920c84f
nn8az <nattn.alt@gmail.com>
2026-03-24 17:11:52 -0400

    Added UUID + JWT on page visit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,34 @@
+import { ServerActionButton } from "@/components/ui/ServerActionButton";
 import { UploadFileButton } from "@/components/ui/UploadFileButton";
+import { cookies } from "next/headers";
+import { jwtVerify } from 'jose';
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const cookie = await cookies();
+  const token = cookie.get("session")?.value;
+  let displayedUUID = "No session cookie found";
+
+  if (token) {
+    try {
+      const { payload } = await jwtVerify(
+        token, 
+        new TextEncoder().encode(process.env.JWT_SECRET)
+      );
+      displayedUUID = payload.userId as string; // This contains your UUID: payload.userId
+    } catch (err) {
+    }
+  }
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-7xl flex-col items-center justify-between py-8 px-16 bg-white dark:bg-black sm:items-start">
         <UploadFileButton />
+        <ServerActionButton />
+        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+          UUID: {displayedUUID}
+        </h1>
+
         <Image
           className="dark:invert"
           src="/next.svg"
---
+++ middleware.tsx
@@ -0,0 +1,35 @@
+import { NextRequest, NextResponse } from "next/server";
+import { SignJWT } from "jose";
+
+const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+
+export async function middleware(request: NextRequest) {
+    console.log("Middleware running for:", request.nextUrl.pathname);
+    const session = request.cookies.get("session");
+
+    if (!session) {
+        const uuid = crypto.randomUUID();
+        const jwt = await new SignJWT({ userId: uuid, isAnonymous: true })
+            .setProtectedHeader({ alg: "HS256" })
+            .setIssuedAt()
+            .setExpirationTime("30d")
+            .sign(secret);
+
+        const response = NextResponse.next();
+        response.cookies.set("session", jwt, {  
+            path: "/",
+            httpOnly: true,
+            sameSite: "lax",
+            maxAge: 60 * 60 * 24 * 30, // 30 days   
+        });
+
+        return response
+    }
+    
+    return NextResponse.next();
+}
+
+// Only run this on page routes, not images or scripts
+export const config = {
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+}
